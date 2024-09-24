fae32866e94296a700c52e36d5822cc7754fca82
erandi2002 <emunasinghe75@gmail.com>
2024-09-25 04:42:29 +0530

    basic database setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,15 @@
 "use server"
 
+import { connect } from "http2";
 import { scrapeAmazonProduct } from "../scraper";
+import { connectToDB } from "../mongoose";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
+        connectToDB();
+
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
         if (!scrapedProduct) return; 
