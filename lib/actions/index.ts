0c19b243016f913631f56bd205da91710fbcc80c
erandi2002 <emunasinghe75@gmail.com>
2024-10-01 14:10:18 +0530

    Database Setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,8 +1,11 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import { connect } from "http2";
 import { scrapeAmazonProduct } from "../scraper";
 import { connectToDB } from "../mongoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -14,7 +17,49 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
         if (!scrapedProduct) return; 
 
+        let product = scrapedProduct;
+
+        const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+        if(existingProduct) {
+            const updatedPriceHistory: any = [
+                ...existingProduct.priceHistory,
+                { price: scrapedProduct.currentPrice }
+            ]
+
+            product  = {
+                ...scrapedProduct,
+                priceHistory: updatedPriceHistory,
+                lowestPrice: getLowestPrice(updatedPriceHistory),
+                highestPrice: getHighestPrice(updatedPriceHistory),
+                averagePrice: getAveragePrice(updatedPriceHistory),
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate(
+            { url: scrapedProduct.url },
+            product,
+            { upsert: true, new: true }
+        );
+
+        revalidatePath(`/products/${newProduct._id}`);
+
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
+    }
+}
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({ _id: productId });
+
+        if(!product) return null; 
+
+        return product;
+        
+    } catch (error) {
+        console.log(error);
     }
 }
